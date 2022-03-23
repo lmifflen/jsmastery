6fc416ff0200e8825843c9f959a0a8dfbc4d81de
lmifflen <lmifflen@gmail.com>
2022-03-23 15:04:29 -0600

    fixed color gradient and button

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -2,8 +2,17 @@ import React from 'react'
 import './header.css';
 const Header = () => {
   return (
-    <div>
-        Header
+    <div className="gpt3__header section__padding" id="home">
+        <div className="gpt3__header-content">
+          <h1 className="gradient__text">Let's Build Something
+              amazing with GPT-3 OpenAI!!!</h1>
+
+          <div className='gpt3__header-content__input'>
+            <input type="email" placeholder='Your email address' />
+            <button type='button'>Get Started</button>
+
+          </div>
+        </div>
     </div>
   )
 }
